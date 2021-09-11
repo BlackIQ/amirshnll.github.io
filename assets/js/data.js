1b5315033739025f5d7cf710d9425108601ef337
Amir Shokri <amirsh.nll@gmail.com>
2021-09-11 14:25:06 +0430

    add some link in my cv

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,7 +5,7 @@ HomeText([
         "profilePic":"assets/img/avatar.jpg",
         "location": "Islamic Republic of Iran",
         "email":"amirsh.nll@gmail.com",
-        "about":"I’m a Master of degree in the Department of Electrical and Computer at the Semnan University doing my research under supervision of Dr. Farzin Yaghmaee. <br/> My main areas of interest are Artificial intelligence (AI), Machine Learning (ML), Natural Language Processing (NLP), Image Memorability. <br /> I like working on open source projects."
+        "about":"I have Master degree of <strong>Artificial Intelligence</strong> in computer and electrical department at the Semnan University, and I'm doing my research underneath cover of supervisor <strong><a target='_blank' href='https://scholar.google.com/citations?user=AM9LzzgAAAAJ' title='Dr. Farzin Yaghmaie'>Dr. Farzin Yaghmaee</a></strong>.<br/> My main areas of interest are Artificial intelligence (AI), Machine Learning (ML), Natural Language Processing (NLP), Image Memorability. <br /> I like working on open source projects."
     }
 ]);
 Links([
@@ -109,42 +109,42 @@ interest([
 Experience([
     {
         "title":"PHP Developer (full-time)",
-        "at":"Cheshmenor",
+        "at":"<a href='https://chenco.ir/' title='Cheshmenor'>Cheshmenor</a>",
         "des": "",
         "startDate":"September 2019",
         "endDate":"currently",
     },
     {
         "title":"Teaching Assistant (part-time)",
-        "at":"Semnan University",
+        "at":"<a href='https://semnan.ac.ir/' title='Semnan University'>Semnan University</a>",
         "des": "image processing Course, Department of Electrical and Computer",
         "startDate":"February 2021",
         "endDate":"June 2021",
     },
     {
         "title":"Teaching Assistant (part-time)",
-        "at":"Semnan University",
+        "at":"<a href='https://semnan.ac.ir/' title='Semnan University'>Semnan University</a>",
         "des": "machine learning Course, Department of Electrical and Computer",
         "startDate":"November 2020",
         "endDate":"January 2021",
     },
     {
         "title":"IT Technician (internship)",
-        "at":"Damavand Mineral Co",
+        "at":"<a href='https://damavandwaters.com/?lang=en' title='Damavand Mineral Co'>Damavand Mineral Co</a>",
         "des": "",
         "startDate":"September 2018",
         "endDate":"December 2018",
     },
     {
         "title":"Web Developer (internship)",
-        "at":"ParsOnline Group",
+        "at":"<a href='https://www.parsonline.com/' title='ParsOnline Group'>ParsOnline Group</a>",
         "des": "",
         "startDate":"June 2015",
         "endDate":"August 2015",
     },
     {
         "title":"PHP Developer (full-time)",
-        "at":"Spiral Design Center",
+        "at":"<a href='http://www.spiraldesign.org/' title='Spiral Design Center'>Spiral Design Center</a>",
         "des": "",
         "startDate":"September 2014",
         "endDate":"January 2015",
@@ -152,19 +152,19 @@ Experience([
 ]);
 Education([
     {
-        "title":"Semnan University",
+        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Semnan_University' title='Semnan University'>Semnan University</a>",
         "study":"Artificial Intelligence and Robotics",
         "grade":"Master's degree, average : 17.69 of 20.00",
         "endDate":"2019 - 2021",
     },
     {
-        "title":"Shamsipour Technical and Vocational University",
+        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Shamsipour_Technical_College' title='Shamsipour Technical and Vocational University'>Shamsipour Technical and Vocational University</a>",
         "study":"Computer Software Technology",
         "grade":"Bachelor's degree, average : 18.13 of 20.00",
         "endDate":"2017 - 2019",
     },
     {
-        "title":"Enghelāb-e Eslāmi Technical College",
+        "title":"<a taget='_blank' href='https://en.wikipedia.org/wiki/Enghel%C4%81b-e_Esl%C4%81mi_Technical_College' title='Enghelāb-e Eslāmi Technical College'>Enghelāb-e Eslāmi Technical College</a>",
         "study":"Computer Software Engineering",
         "grade":"Associate's degree, average : 17.06 of 20.00",
         "endDate":"2014 - 2017",
@@ -193,7 +193,7 @@ Project([
         "title":"right to be forgotten (RTBF) Directory <span class='fal fa-star'></span> 70 star",
         "link":"https://rtbf.ir",
         "href":"https://rtbf.ir",
-        "des":"A directory of direct links to delete your account from iranian web services. | github repository : <strong>amirshnll/rtbf.ir</strong>"
+        "des":"A directory of direct links to delete your account from iranian web services. | github repository : <strong><a href='https://github.com/amirshnll/rtbf.ir' title='amirshnll/rtbf.ir'>amirshnll/rtbf.ir</a></strong>"
     },
 ]);
 Hobby([
